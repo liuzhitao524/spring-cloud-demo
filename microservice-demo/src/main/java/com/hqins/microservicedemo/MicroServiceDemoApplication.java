62f32872835400058d5a6a33d8644b00e86e6ea2
Liu Zhitao <413139276@qq.com>
2018-07-16 11:23:10 +0800

    microservice

--- microservice-demo/src/main/java/com/hqins/microservicedemo/MicroServiceDemoApplication.java
+++ microservice-demo/src/main/java/com/hqins/microservicedemo/MicroServiceDemoApplication.java
@@ -1,9 +1,12 @@
 package com.hqins.microservicedemo;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
+
 
 @SpringBootApplication
 @EnableEurekaClient
