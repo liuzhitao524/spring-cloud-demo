025252dc9fc5e72a2b421011e770cef786e58f3b
Liu Zhitao <413139276@qq.com>
2018-07-17 14:23:36 +0800

    spring cloud config

---
+++ config-client/src/main/java/com/hqins/configclient/controller/ConfigDemoController.java
@@ -0,0 +1,19 @@
+package com.hqins.configclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConfigDemoController {
+
+    @Value("${name}")
+    String name;
+    @RequestMapping(value="/name",method=RequestMethod.GET)
+    public String getName(){
+        return name;
+    }
+
+
+}
